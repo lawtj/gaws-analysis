f5829b432bca8cb2dcf1232f029b71aa22e2fd77
lawtj <tyler.law@gmail.com>
2022-07-13 10:22:01 -0700

    update region comparison

--- analysis.py
+++ analysis.py
@@ -11,7 +11,7 @@ pio.templates.default = 'seaborn'
 df = pd.read_csv('analysis.csv')
 
 #Group for individual country analysis
-dfg = df.groupby('country').mean()
+dfg = df.groupby(['country','Region']).mean()
 dfg = dfg.reset_index()
 provnumbers = dfg[['country', 'totalpap','totalpap_cap', 'totalnpap','totalnpap_cap', 'totalproviders', 'totalproviders_cap']].round(0)
 
@@ -29,7 +29,7 @@ who = who.reset_index()
 
 st.title('GAWS analysis')
 
-st.subheader('# of providers per 100,00 people (per capita)')
+st.subheader('1. # of providers per 100,00 people (per capita)')
 
 ###########################################
 col1, col2 = st.columns(2)
@@ -46,11 +46,11 @@ with col2:
     st.plotly_chart(fig)
 
 ###########################################
-st.write('### Table of absolute and per capita provider numbers')
+st.write('#### Raw data of absolute and per capita provider numbers')
 st.dataframe(provnumbers)
 
 ###########################################
-st.subheader('% of providers that are women')
+st.subheader('2. % of providers that are women')
 
 col1, col2 = st.columns(2)
 with col1:
@@ -67,3 +67,69 @@ with col2:
     st.caption('Blue = Physicians, Orange = nonphysicisans')
 
 ###########################################
+########### comparison to previous GAWS
+
+st.header('Comparison to previous GAWS')
+st.write('This compares the current GAWS per capita numbers vs the GAWS per capita numbers in 2015. A number greater than 1 indicates increase by x% (eg. 1.5 is a 1.5x increase, or 150%). A number less than 1 indicates a decrease. ')
+
+dfg=dfg.sort_values(by='physiciancap_diff')
+
+######europe
+st.write('#### European Region')
+col1, col2 = st.columns(2)
+with col1:
+    fig = px.bar(x=dfg[dfg['Region']=='European Region']['country'], y=dfg[dfg['Region']=='European Region']['physiciancap_diff'])
+    st.plotly_chart(fig)
+
+with col2:
+    dfg[dfg['Region']=='European Region'][['country', 'totalpap_cap', 'physicians2015', 'physiciancap_diff']]
+
+#########africa
+st.write('#### African Region')
+col1, col2 = st.columns(2)
+with col1:
+    fig = px.bar(x=dfg[dfg['Region']=='African Region']['country'], y=dfg[dfg['Region']=='African Region']['physiciancap_diff'])
+    st.plotly_chart(fig)
+    st.caption('Tanzania is a clear outlier, with almost 26x growth since 2015?')
+
+with col2:
+    dfg[dfg['Region']=='African Region'][['country', 'totalpap_cap', 'physicians2015', 'physiciancap_diff']]
+
+############ americas
+st.write('#### Americas Region')
+col1, col2 = st.columns(2)
+with col1:
+    fig = px.bar(x=dfg[dfg['Region']=='Region of the Americas']['country'], y=dfg[dfg['Region']=='Region of the Americas']['physiciancap_diff'])
+    st.plotly_chart(fig)
+
+with col2:
+    dfg[dfg['Region']=='Region of the Americas'][['country', 'totalpap_cap', 'physicians2015', 'physiciancap_diff']]
+
+########## EMRO
+st.write('#### Eastern Mediterranean Region')
+col1, col2 = st.columns(2)
+with col1:
+    fig = px.bar(x=dfg[dfg['Region']=='Eastern Mediterranean Region']['country'], y=dfg[dfg['Region']=='Eastern Mediterranean Region']['physiciancap_diff'])
+    st.plotly_chart(fig)
+
+with col2:
+    st.dataframe(dfg[dfg['Region']=='Eastern Mediterranean Region'][['country', 'totalpap_cap', 'physicians2015', 'physiciancap_diff']])
+
+########## SEASIA
+st.write('#### South-East Asia Region')
+col1, col2 = st.columns(2)
+with col1:
+    fig = px.bar(x=dfg[dfg['Region']=='South-East Asia Region']['country'], y=dfg[dfg['Region']=='South-East Asia Region']['physiciancap_diff'])
+    st.plotly_chart(fig)
+
+with col2:
+    st.dataframe(dfg[dfg['Region']=='South-East Asia Region'][['country', 'totalpap_cap', 'physicians2015', 'physiciancap_diff']])
+
+st.write('#### Western Pacific Region')
+col1, col2 = st.columns(2)
+with col1:
+    fig = px.bar(x=dfg[dfg['Region']=='Western Pacific Region']['country'], y=dfg[dfg['Region']=='Western Pacific Region']['physiciancap_diff'])
+    st.plotly_chart(fig)
+
+with col2:
+    st.dataframe(dfg[dfg['Region']=='Western Pacific Region'][['country', 'totalpap_cap', 'physicians2015', 'physiciancap_diff']])
